1f7680e5e04bdeee1fb8c3d563698f20bc6d0e5c
newtaDev <newta.dev@gmail.com>
2022-04-20 22:12:36 +0530

    changes in ci

--- tools/verify_pub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "--------------- Format code ---------------"
-flutter format .
-echo "--------------- --dry-run ---------------"
-dart pub publish --dry-run
-echo "--------------- verify pub score ---------------"
-pana . --no-warning
-
